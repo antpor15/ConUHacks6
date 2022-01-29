01b461263275820e97efe916d428935495cf7dde
Dina Khalesi <dinakhalesit@gmail.com>
2022-01-29 16:29:52 -0500

    general css

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,14 +2,24 @@ import React from 'react';
 import "../css/navbar.css";
 import { AiFillHome } from "react-icons/ai";
 import { GiHamburgerMenu } from "react-icons/gi"
+import { AiFillGithub } from "react-icons/ai";
 
 export const Navbar = () => {
     return <div id="navbar">
-        <div>
-            <button><AiFillHome></AiFillHome></button>
-            <button><GiHamburgerMenu></GiHamburgerMenu></button>
+        <div id="top-of-nav">
+            <button id="invisible"><AiFillHome size={22}></AiFillHome></button>
+            <button className="icons"><AiFillHome size={22}></AiFillHome></button>
+            <button className="icons"><GiHamburgerMenu size={22}></GiHamburgerMenu></button>
         </div>
-        <button>Create Fund</button>
-        <button>Discovery</button>
+        <div id="nav-features">
+            <button className="nav-button">Create Fund</button>
+            <button className="nav-button">Discovery</button>
+        </div>
+
+        <div id="bottom-of-nav">
+            <button className="icons">
+                <AiFillGithub size={28}></AiFillGithub>
+            </button>
+        </div>
     </div>;
 };
