b3adcc7421542504e8ba58449315fbcec7e62ae3
Dina Khalesi <dinakhalesit@gmail.com>
2022-01-29 12:18:43 -0500

    finalize skeleton

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Route, Routes } from "react-router";
+
+const Router = (props) => {
+    return (
+        <Routes>
+            {/* <Route path="/" exact element={<Home tabChange={props.tabChange} />} /> */}
+
+        </Routes>
+    )
+}
+
+export default Router
