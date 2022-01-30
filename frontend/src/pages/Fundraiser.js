83b7a3a2f891b41d2e5ce2b1c9a62f542f914cd5
Alex <stathis.alexander@gmail.com>
2022-01-30 00:24:51 -0500

    fundraiser struture

--- frontend/src/pages/Fundraiser.js
+++ frontend/src/pages/Fundraiser.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {useParams} from "react-router";
 import '../css/fundraiser.css'
 
-const Fundraiser = () => {
+const Fundraiser = (fundraiser) => {
     const route = useParams()
 
     console.log(route)
@@ -15,8 +15,12 @@ const Fundraiser = () => {
                          src={"https://cdn.onebauer.media/one/media/5ddc/ffc7/0a4e/c494/e8f7/62f2/star-wars-rise-skywalker-kylo-rey.jpg?format=jpg&quality=80&width=1400&ratio=16-9&resize=aspectfill"}/>
                 </div>
 
-                <div className={'description'}>
-
+                <div className={'content'}>
+                    <h3>Title: {fundraiser.title}</h3>
+                    <h3>Description: {fundraiser.title}</h3>
+                    <h3>Goal: {fundraiser.title}</h3>
+                    <h3>Endtime: {fundraiser.title}</h3>
+                    <h3>URL: <a href={'/'}>test</a></h3>
                 </div>
             </div>
         </div>
