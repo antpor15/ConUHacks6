166503b07e0fae8ebb4b243ec6ffb1851d952ad7
Dina Khalesi <dinakhalesit@gmail.com>
2022-01-30 06:33:51 -0500

    prepared create fund

--- frontend/src/pages/CreateFund.js
+++ frontend/src/pages/CreateFund.js
@@ -6,92 +6,117 @@ import styled from 'styled-components'
 import '../css/createFund.css';
 
 export function CreateFund() {
-  const form = useForm({
-    initialValues: {
-      campaignTitle: "",
-      campaignDeadline: "",
-      description: "",
-      goalAchieve: "",
-      keywords: "",
-      voteOptions: "",
-    },
+    const form = useForm({
+        initialValues: {
+            campaignTitle: "",
+            campaignDeadline: "",
+            description: "",
+            goalAchieve: "",
+            keywords: "",
+            voteOptions: "",
+        }
+    });
 
-    validationRules: {
-      email: (value) => /^\S+@\S+$/.test(value),
-    },
-  });
+    const [keywords, setKeywords] = useState([]);
+    const [formData, setFormData] = useState({});
 
-  const [keywords, setKeywords] = useState([]);
+    const handleSubmit = (values) => {
+        console.log(values)
+        if (values.campaignTitle === '' || values.description === '' || values.goalAchieve === '' || values.keywords.length === 0 || values.voteOptions === '') {
+            alert("Please fill in the required areas")
+            return;
+        }
+        const voteOptions = splitString(values.voteOptions, 4);
+        const newFormData = {
+            "voteOptionsAmount": voteOptions.length,
+            "title": values.campaignTitle,
+            "description": values.description,
+            "goalInGWei": values.goalAchieve,
+            "keywords": splitString(values.keywords, 10),
+            "endDateTime": values.campaignDeadline, // TOO-DO: convert to UTC unix timestamp
+            "picURL": values.image,
+            "vote": voteOptions
+        }
 
-  const handleSubmit = (values) => {
-    console.log(values)
-    if (values.campaignTitle === '' || values.description === '' || values.goalAchieve === '' || values.keywords.length === [''] || values.voteOptions === '') {
-      alert("Please fill in the required areas")
-      return;
+        setFormData(newFormData);
+
+        console.log(newFormData)
     }
-    splitString(values.voteOptions)
-  }
 
-  function splitString(voteOptions) {
-    const optionArray = voteOptions.split(",")
-    // console.log(optionArray)
-  }
+    function splitString(voteOptions, maxAmount) {
+        return voteOptions.split(",", maxAmount);
+    }
 
-  return (
-    <Flex>
-      <Form onSubmit={form.onSubmit((values) => {
-        handleSubmit(values)
-      })}>
-        <TextInput
-          required
-          label="Campaign Title"
-          placeholder="Your Campaign Title"
+    return (
+        <Flex>
+            <Form onSubmit={form.onSubmit((values) => {
+                handleSubmit(values)
+            })}>
+                <TextInput
+                    required
+                    label="Campaign Title"
+                    placeholder="Your Campaign Title"
 
-          {...form.getInputProps('campaignTitle')}
-        />
-        <TextInput
-          required
-          label="Campaign Deadline"
-          input type="datetime-local"
-          {...form.getInputProps('campaignDeadline')}
-        />
-        <Textarea
-          required
-          label="Description"
-          placeholder='Describe your campaign'
-          {...form.getInputProps('description')}
-        />
-        <TextInput
-          required
-          label="Goal to Achieve"
-          placeholder='0.00000000 ETH'
-          {...form.getInputProps('goalAchieve')}
-        />
-        <Select
-          required
-          label="Keywords"
-          data={keywords}
-          placeholder='Input keywords that represent your campaign'
-          creatable
-          searchable
-          getCreateLabel={(query) => `+ Create ${query}`}
-          onCreate={(query) => setKeywords((current) => [...current, query])}
-          {...form.getInputProps('keywords')}
-        />
-        <Textarea
-          required
-          label="Vote Options"
-          placeholder='Input candidates here. (seperate each entry by a comma.)'
-          autosize
-          minRows={2}
-          maxRows={4}
-          {...form.getInputProps('voteOptions')}
-        />
-        <CButton
-          type="submit">Submit</CButton>
-      </Form>
-    </Flex>
-  );
+                    {...form.getInputProps('campaignTitle')}
+                />
+                <TextInput
+                    required
+                    label="Campaign Deadline"
+                    input type="datetime-local"
+                    {...form.getInputProps('campaignDeadline')}
+                />
+                <Textarea
+                    required
+                    label="Description"
+                    placeholder='Describe your campaign'
+                    {...form.getInputProps('description')}
+                />
+                <TextInput
+                    required
+                    label="Goal to Achieve"
+                    placeholder='0.00000000 ETH'
+                    {...form.getInputProps('goalAchieve')}
+                />
+                <TextInput
+                    required
+                    label="Image URL"
+                    placeholder='www.image.com'
+                    {...form.getInputProps('image')}
+                />
+                <Select
+                    required
+                    label="Keywords"
+                    data={keywords}
+                    placeholder='Input keywords that represent your campaign'
+                    creatable
+                    searchable
+                    getCreateLabel={(query) => `+ Create ${query}`}
+                    onCreate={(query) => { setKeywords((current) => [...current, query]) }}
+                    {...form.getInputProps('keywords')}
+                />
+                <Textarea
+                    required
+                    label="Keywords"
+                    placeholder='Input keywords that represent your campaign. (seperate each entry by a comma.)'
+                    autosize
+                    minRows={2}
+                    maxRows={4}
+                    {...form.getInputProps('keywords')}
+                />
+                <Textarea
+                    required
+                    label="Vote Options"
+                    placeholder='Input candidates here. (seperate each entry by a comma.)'
+                    autosize
+                    minRows={2}
+                    maxRows={4}
+                    {...form.getInputProps('voteOptions')}
+                />
+                <CButton
+                    type="submit">Submit</CButton>
+            </Form>
+        </Flex>
+    );
 }
 
 const CButton = styled(Button)`
