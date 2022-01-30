cf2560010c08f101316a946034165042fa789006
Alex <stathis.alexander@gmail.com>
2022-01-29 23:57:29 -0500

    Fix ish navbar

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router";
 import { CreateFund } from './CreateFund';
 import { Discover } from './Discover';
 import { Home } from './Home'
+import Fundraiser from "./Fundraiser";
 
 const Router = (props) => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/createFundraiser" element={<CreateFund />} />
+            <Route path="/fundraiser" element={<Fundraiser/>} />
             <Route path="/discover" element={<Discover />} />
         </Routes>
     )
