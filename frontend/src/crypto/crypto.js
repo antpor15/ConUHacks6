73ad82a6f3348bca191feeb6d38fee8f51ee1df8
Nicolas MacBeth <nicolasmacbeth@gmail.com>
2022-01-30 05:16:07 -0500

    examples

--- frontend/src/crypto/crypto.js
+++ frontend/src/crypto/crypto.js
@@ -98,19 +98,21 @@ const convertGWeiToEth = (GWei) => {
     return ethers.utils.formatEther((GWei * 1000000000).toString()); // Uses Big number?
 }
 
-getProvider().then((prov) => {
-    getAccount(prov).then((d) => {console.log(d)});
-    let cont = getReadOnlyContract(prov);
-    getAllCampaigns(cont).then((res) => {console.log(res)});
-    getCampaignsCount(cont).then((res) => {console.log(res.toString())});
-    getCampaignMetadata(cont, 3).then(res => {console.log(res)})
-    getAllCampaigns(cont).then(res => {console.log(res)});
+// Examples:
 
-    getSigner(prov).then((s) => {
-        // let contRW = getReadWriteContract(s);
-        // voteAndBack(contRW, 0, 0, "0.000001").then(res=>console.log(res));
-        // claim(contRW, 0).then(res=>console.log(res));
-        // let d = {voteOptionsAmount: 2, title: "Get Darth Vader a new face", description: "You know the deal, I lost my face. Please help me by backing me during this project. You can help me choose the type of nose I have!", goalInGWei: 11036459883, currentFundingInGWei: 0, keywords: ["darth", "vader"], endDateTime: 1643506711, picURL: "https://lumiere-a.akamaihd.net/v1/images/607598d0230e6a00018e21b2-image_354b1b56.jpeg?region=0%2C48%2C1536%2C768&width=960", creator: "0x0000000000000000000000000000000000000000"};
-        // createCampaign(contRW, [{title: "Big nose", votes: 0}, {title: "Small nose", votes: 0}], d).then(() => {console.log("done creating?")})
-    });
-});
+// getProvider().then((prov) => {
+//     getAccount(prov).then((d) => {console.log(d)});
+//     let cont = getReadOnlyContract(prov);
+//     getAllCampaigns(cont).then((res) => {console.log(res)});
+//     getCampaignsCount(cont).then((res) => {console.log(res.toString())});
+//     getCampaignMetadata(cont, 3).then(res => {console.log(res)})
+//     getAllCampaigns(cont).then(res => {console.log(res)});
+
+//     getSigner(prov).then((s) => {
+//         let contRW = getReadWriteContract(s);
+//         voteAndBack(contRW, 0, 0, "0.000001").then(res=>console.log(res));
+//         claim(contRW, 0).then(res=>console.log(res));
+//         let d = {voteOptionsAmount: 2, title: "Get Darth Vader a new face", description: "You know the deal, I lost my face. Please help me by backing me during this project. You can help me choose the type of nose I have!", goalInGWei: 11036459883, currentFundingInGWei: 0, keywords: ["darth", "vader"], endDateTime: 1643506711, picURL: "https://lumiere-a.akamaihd.net/v1/images/607598d0230e6a00018e21b2-image_354b1b56.jpeg?region=0%2C48%2C1536%2C768&width=960", creator: "0x0000000000000000000000000000000000000000"};
+//         createCampaign(contRW, [{title: "Big nose", votes: 0}, {title: "Small nose", votes: 0}], d).then(() => {console.log("done creating?")})
+//     });
+// });
