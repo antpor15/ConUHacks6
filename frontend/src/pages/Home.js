cf4a93471face9a025d67bdaf15b08b6d5edf8aa
Dina Khalesi <dinakhalesit@gmail.com>
2022-01-30 07:48:38 -0500

    homepage and fundraiser

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,40 +1,45 @@
 import React from 'react';
 import styled from 'styled-components'
-import { useParams } from "react-router";
-import { Container } from '@mantine/core';
 
 export const Home = () => {
-    return <flex>
-        <Container>
-            <text>
-                A long time ago, in a metaverse far far away... Decentralized cryptofunding was climbing its way to the top!
-                Here are some great projects waiting to be discovered and backed by you,
-                the people of the future.
-            </text>
-        </Container>
-    </flex >;
+  return <Flex>
+    <Texth1>Star Funds</Texth1>
+    <Container>
+      <Text>
+        A long time ago, in a metaverse far far away... Decentralized cryptofunding was climbing its way to the top!
+        Here are some great projects waiting to be discovered and backed by you,
+        the people of the future.
+      </Text>
+    </Container>
+  </Flex>
+  return <div></div>
 };
 
-const Text = styled.p`
+const Texth1 = styled.h1`
+color: white;
+  padding-left: 24px;
+  max-width: 30rem;
+  font-size:80;
+  color: #FFE81F;
+`
+const Text = styled.h2`
   color: white;
   padding-left: 24px;
   max-width: 30rem;
-  `
-const Flex = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100vw;
-  padding-top: 60px;
+  font-size:80;
+  color: #FFE81F;
   `
 
-Container = styled.div`
+
+const Container = styled.div`
+
   width: 50%;
   background-color: #292929;
   opacity: 0.8;
   padding: 2rem;
   display: flex;
   flex-wrap: wrap;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   gap: 2rem;
@@ -42,3 +47,12 @@ Container = styled.div`
   max-width: 1200px;
   margin-bottom: 2rem;
   `
+
+const Flex = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    padding-top: 60px;
+`
